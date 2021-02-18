693d752cde1bf28b50c69fa49f4bbaea34b189dd
Muhammad Wildan <wildanrgssport@gmail.com>
2021-02-18 15:41:41 +0700

    add callback to user

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,7 @@
-import requests
+import json
 import os
+import requests
+import time
 from dotenv import load_dotenv, find_dotenv
 
 # load .env file
@@ -15,15 +17,41 @@ def sendMessage(chatID, text):
     return message
 
 
-# function for check user profile photos
-def checkPhotoProfile():
-    parameter = {'user_id': chat_ID}
-    sendMessage(chat_ID, "halo kawan!")  # send message "halo kawan!"
-    getUserPhotoProfile = requests.post(
-        bot_token + 'getUserProfilePhotos', data=parameter)
-    sendMessage(chat_ID, getUserPhotoProfile)  # send message from return API
+# # function for check user profile photos (temporary disabled)
+# def checkPhotoProfile():
+#     parameter = {'user_id': chat_ID}
+#     sendMessage(chat_ID, "halo kawan!")  # send message "halo kawan!"
+#     getUserPhotoProfile = requests.post(
+#         bot_token + 'getUserProfilePhotos', data=parameter)
+#     sendMessage(chat_ID, getUserPhotoProfile)  # send message from return API
+
+def job():
+    while True:
+        message = requests.get(bot_token + 'getUpdates')
+        messageJSON = json.loads(message.text)
+        try:
+            messageText = messageJSON['result'][-1]['message']['text']
+            userName = messageJSON['result'][-1]['message']['from']['first_name']
+            if messageText == "/start@surabayapy_bot":
+                sendMessage(
+                    chat_ID, "Halo,{}! tekan masukkan perintah /antispam untuk mengaktifkan fitur antispam :D".format(userName))
+
+                while True:
+                    message = requests.get(bot_token + 'getUpdates')
+                    messageJSON = json.loads(message.text)
+                    messageText = messageJSON['result'][-1]['message']['text']
+                    print("menunggu perintah")
+                    time.sleep(1)
+                    if messageText == "/antispam@surabayapy_bot":
+                        sendMessage(chat_ID, "fitur antispam telah diaktifkan")
+                        return
+
+        except KeyError:
+            pass
 
 
 # for a while, the main program like this
 if __name__ == "__main__":
-    checkPhotoProfile()
+    sendMessage(chat_ID, "bot activated . .")
+    sendMessage(chat_ID, "kirimkan perintah /start untuk memulai")
+    job()
